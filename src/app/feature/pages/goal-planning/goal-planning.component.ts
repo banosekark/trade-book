519a8f6e3b08a79a1812bc2843c198fe681b2edf
Kamlesh <banosekar.kamlesh@gmail.com>
2024-08-26 16:22:38 +0530

    old changes

--- src/app/feature/pages/goal-planning/goal-planning.component.ts
+++ src/app/feature/pages/goal-planning/goal-planning.component.ts
@@ -4,6 +4,12 @@ import { GoalTableComponent } from './goal-table/goal-table.component';
 import { GenerateGoalFormComponent } from './generate-goal-form/generate-goal-form.component';
 import { MatButtonModule } from '@angular/material/button';
 import { TradePlanService } from '../../services/trade-plan.service';
+import {
+  MatDialog,
+  MatDialogModule,
+  MatDialogRef,
+} from '@angular/material/dialog';
+import { MatFormFieldModule } from '@angular/material/form-field';
 
 @Component({
   selector: 'app-goal-planning',
@@ -13,6 +19,8 @@ import { TradePlanService } from '../../services/trade-plan.service';
     GoalTableComponent,
     GenerateGoalFormComponent,
     MatButtonModule,
+    MatDialogModule,
+    MatFormFieldModule,
   ],
   templateUrl: './goal-planning.component.html',
   styleUrl: './goal-planning.component.scss',
@@ -20,14 +28,32 @@ import { TradePlanService } from '../../services/trade-plan.service';
 export class GoalPlanningComponent {
   goalTableData: any;
 
-  constructor(private tradePlanService: TradePlanService) {}
+  constructor(
+    private tradePlanService: TradePlanService,
+    public dialog: MatDialog
+  ) {}
 
   ngOnInit() {}
 
-  savePlan() {
-    this.tradePlanService.settingsData$.subscribe((data) => {
-      this.goalTableData = data;
-      console.log('this.goalTableData', this.goalTableData);
+  savePlan(): void {
+    this.dialog.open(DialogAnimationsExampleDialog, {
+      width: '250px',
     });
   }
+}
+
+@Component({
+  selector: 'dialog-animations-example-dialog',
+  templateUrl: './dialog.html',
+  standalone: true,
+  imports: [
+    MatDialogModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatButtonModule,
+    MatDialogModule,
+  ],
+})
+export class DialogAnimationsExampleDialog {
+  constructor(public dialogRef: MatDialogRef<DialogAnimationsExampleDialog>) {}
 }
